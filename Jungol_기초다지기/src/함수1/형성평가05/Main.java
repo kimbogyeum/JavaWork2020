d7179ae05f3811bc2a36d2c50ed970fe88acd443
kimbogyeum <kimbogyeum1@naver.com>
2020-05-06 23:51:04 +0900

    5/6과제 commit

--- Jungol_기초다지기/src/함수1/형성평가05/Main.java
+++ Jungol_기초다지기/src/함수1/형성평가05/Main.java
@@ -17,17 +17,42 @@ import java.util.Scanner;
  */
 public class Main {
 
+
 	public static void main(String[] args) {
-		
-		int arr;
-		Scanner sc=new Scanner(System.in);
-		
-		for (int i = 0; i < 3; i++) {
-			
-		}
 
 		
+		int arr[][]=new int[4][4];
+		int sum=0;
+		Scanner sc=new Scanner(System.in);
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				arr[i][j]=sc.nextInt();
+				sum=sum+arr[i][j];
+			}
+			arr[i][3]=sum;
+			sum=0;
+		}
+		
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				sum=sum+arr[j][i];
+			}
+			arr[3][i]=sum;
+			sum=0;
+		}
+		
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				System.out.print(arr[i][j]+" ");
+			}
+			System.out.println();
+		}
+		
 		sc.close();
+
+		
+		
 	}
 
 }
