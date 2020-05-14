3247ba8dd50232d0423bb516c1eed39cfeadbb98
kimbogyeum <kimbogyeum1@naver.com>
2020-05-15 03:19:44 +0900

    5/15 새벽커밋

--- Lec_WEB/WebContent/myweb/JS.js
+++ Lec_WEB/WebContent/myweb/JS.js
@@ -1,23 +1,4 @@
 
-
-
-$('document').ready(function(){
-    $('.menu1').fixedMenu();
-});
-
-(function($){
-    $.fn.fixedMenu=function(){
-        return this.each(function(){
-            var menu11= $(this);
-            menu11.find('ul li > a').bind('hover',function(){
-            if ($(this).parent().hasClass('active')){
-                $(this).parent().removeClass('active');
-            }
-            else{
-                $(this).parent().parent().find('.active').removeClass('active');
-                $(this).parent().addClass('active');
-            }
-            })
-        });
-    }
-})(jQuery);
+$(".menu1 ul li").hover(function(){
+  $(this).find("ul").stop().fadeToggle(300);
+  });
