d7c6066522627641c2112525afbcdb5a67c09173
kimbogyeum <kimbogyeum1@naver.com>
2020-05-17 18:50:32 +0900

    5/17 commit

--- Lec_WEB/WebContent/myweb/JS.js
+++ Lec_WEB/WebContent/myweb/JS.js
@@ -18,6 +18,9 @@ $(function(){
 
 
     function slideImg(){
+		$("#main").css('background-image','url(img/main'+check+'.jpg');
+		$('#slide_img img.pic1').stop(true).css({'opacity': 1}).animate({'opacity': 1}, 1500);
+
     	$('#slide_img img.pic1').attr('src', 'img/slide_'+check+'.jpg');
 		$('#slide_img img.pic1').stop(true).css({'opacity': 0.3}).animate({'opacity': 1}, 1500);
 		
@@ -26,39 +29,9 @@ $(function(){
     }
 	
 
-// email 패턴에 대한 정규표현식
-	var emailPat = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
-	var passwordPat=/^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,16}$/;
 
 
-
-	function chkSubmit6(){
-		var frm=document.classform;
-		var email=frm.email.value.trim();
-		var password=frm.password.value.trim();
-
-
-		document.getElementById("passwarn").innerHTML="";
-		document.getElementById("emailwarn").innerHTML="";
-
-		//정규표현식 객체의 test() : 패턴매칭 여부. 
-		if(!passwordPat.test(password)){
-			document.getElementById("passwarn").innerHTML="유효하지 않은 비밀번호";
-			frm.url.focus();
-			return false;
-		}
-
-		if(!emailPat.test(email)){
-			document.getElementById("emailwarn").innerHTML="잘못된 email";
-			frm.email.focus();
-			return false;
-		}
-	return true;
-	}
-
-
-
-
+	
 	// form 팝업창
 		var Btn = document.getElementById("Btn");
 		var modal = document.getElementById("id01");
@@ -78,6 +51,24 @@ $(function(){
 			}
 		}
 		
-    
+	// //팝업창 체크표시여부
+	$(".cc1").hide();
+	$(".cc2").hide();
+	$(".cc3").hide();
+	$(".cc4").hide();
+	$(".cc5").hide();
+	$(".cc6").hide();
+	$(".cc7").hide();
+	$(".cc8").hide();
 
+	$("._1").click(function(){$(".cc1").show();});
+	$("._2").click(function(){$(".cc2").show();});	
+	$("._3").click(function(){$(".cc3").show();});	
+	$("._4").click(function(){$(".cc4").show();});	
+	$("._5").click(function(){$(".cc5").show();});	
+	$("._6").click(function(){$(".cc6").show();});	
+	$("._7").click(function(){$(".cc7").show();});	
+	$("._8").click(function(){$(".cc8").show();});
 });
+
+
