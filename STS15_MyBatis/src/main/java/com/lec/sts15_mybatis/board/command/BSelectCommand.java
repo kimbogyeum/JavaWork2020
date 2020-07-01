4f3ae0fcc81be43dbf63848aec32e1f26593df7f
kimbogyeum <kimbogyeum1@naver.com>
2020-07-01 17:21:28 +0900

    7/1 commit

---
+++ STS15_MyBatis/src/main/java/com/lec/sts15_mybatis/board/command/BSelectCommand.java
@@ -0,0 +1,47 @@
+package com.lec.sts15_mybatis.board.command;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import org.springframework.ui.Model;
+
+import com.lec.sts15_mybatis.board.C;
+import com.lec.sts15_mybatis.board.beans.BWriteDAO;
+import com.lec.sts15_mybatis.board.beans.BWriteDTO;
+import com.lec.sts15_mybatis.board.beans.IWriteDAO;
+
+public class BSelectCommand implements BCommand {
+
+	@Override
+	public void execute(Model model) {
+		Map<String, Object> map = model.asMap();
+		int uid = (Integer)map.get("uid");
+		
+		
+//		BWriteDAO dao = new BWriteDAO();
+//		BWriteDTO dto = dao.selectByUid(uid);
+//		model.addAttribute("list", Arrays.asList(dto));
+		
+		//myBatis 사용
+		IWriteDAO dao=C.sqlSession.getMapper(IWriteDAO.class);
+		model.addAttribute("list",Arrays.asList(dao.selectByUid(uid)));
+		
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
